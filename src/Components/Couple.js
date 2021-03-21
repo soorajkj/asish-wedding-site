c901a05afdab05f6ad50c68b23a28844ffc6e5a7
soorajkj <soorajkj46@gmail.com>
2021-03-22 00:11:42 +0530

    first commit

--- src/Components/Couple.js
+++ src/Components/Couple.js
@@ -1,45 +1,34 @@
 import React from "react";
-
+import CoupleComponent from "./CoupleComponent";
 const Couple = () => {
   return (
     <section className="couple">
       <div>
         <h2>Happy Couple</h2>
         <div className="couple-tile">
-          <div className="bride">
-            <img
-              src="http://lavelo-react.wpocean.com/static/media/img-2.8fa42fa6.jpg"
-              alt=""
-              className="tile-image"
-            />
-            <h3 className="name">Alphonsa Kurian</h3>
-            <p className="detail">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium quod rerum fugiat nam cumque itaque aperiam est sunt
-              iste nisi facilis animi, perspiciatis, voluptate dolorum sequi
-              modi! Ullam, quae nesciunt?
-            </p>
-          </div>
+          <CoupleComponent
+            type="bride"
+            img="http://lavelo-react.wpocean.com/static/media/img-2.8fa42fa6.jpg"
+            name="Alphonsa Kurian"
+            detail=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quod
+            rerum fugiat nam cumque itaque aperiam est sunt iste nisi facilis animi,
+            perspiciatis, voluptate dolorum sequi modi! Ullam, quae nesciunt?"
+          />
+
           <div className="tile-seperator">
             <img
               src="http://lavelo-react.wpocean.com/static/media/2.3b18046c.png"
               alt=""
             />
           </div>
-          <div className="groom">
-            <img
-              src="http://lavelo-react.wpocean.com/static/media/img-2.8fa42fa6.jpg"
-              alt=""
-              className="tile-image"
-            />
-            <h3 className="name">Asish K Antony</h3>
-            <p className="detail">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium quod rerum fugiat nam cumque itaque aperiam est sunt
-              iste nisi facilis animi, perspiciatis, voluptate dolorum sequi
-              modi! Ullam, quae nesciunt?
-            </p>
-          </div>
+          <CoupleComponent
+            type="groom"
+            img="http://lavelo-react.wpocean.com/static/media/img-2.8fa42fa6.jpg"
+            name="Asish K Antony"
+            detail="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quod
+            rerum fugiat nam cumque itaque aperiam est sunt iste nisi facilis animi,
+            perspiciatis, voluptate dolorum sequi modi! Ullam, quae nesciunt?"
+          />
         </div>
       </div>
     </section>
